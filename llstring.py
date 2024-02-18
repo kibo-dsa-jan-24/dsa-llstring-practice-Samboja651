44cda29d13b7b6c82e0c720d1ed5f87bd5721c58
Samboja651 <waribekihiko651@gmail.com>
2024-02-18 20:52:20 +0300

    my_best_trial

--- llstring.py
+++ llstring.py
@@ -37,16 +37,81 @@ class LLString:
         return s
 
     def print_every_other(self):
+        return self.__print_every_other(0, self.head)
         pass
 
+    def __print_every_other(self,counter, node):
+        if node is None:
+            return print()
+        else:
+            if counter % 2 == 0:
+                print(node.val, end='')
+                return self.__print_every_other(counter + 1, node.next)
+            else:
+                return self.__print_every_other(counter + 1, node.next)
+            
+
     def char_at(self, i):
+        trav = self.head
+        prev = None
+        num = 0
+        if trav == None:
+            return None
+        
+        while num < i:
+            prev = trav
+            trav = trav.next
+            num += 1
+            if trav == None:
+                return None
+        return trav.val
         pass
 
     def concat(self, other_llstring):
-        pass
+        current_str = ''
+        trav = self.head
+        while trav is not None:
+            current_str += trav.val
+            trav = trav.next
+        
+        incoming_str = ''
+        new_trav = other_llstring.head
+        while new_trav is not None:
+            incoming_str += new_trav.val
+            new_trav = new_trav.next
+        
+        return current_str + incoming_str
+
 
     def reverse(self):
+        head = self.head.next
+        prev = self.head
+        temp = prev
+        while prev is not None:
+            prev = head
+            head = temp
+            prev = prev.next
+            head = head.next
+            if head == None:
+                head = prev
+        
         pass
 
     def index_of(self, c):
-        pass
+        return self.__index_of(c, self.head)
+    
+    def __index_of(self, c, node):   
+        
+        if node == None:
+            return -1
+        
+        if node.val == c:
+            return 0
+        else:
+            return 1 + self.__index_of(c, node.next)
+    
+
+
+mylist = LLString('hello')
+mylist2 = LLString('orange')
+print(mylist.concat(mylist2))
